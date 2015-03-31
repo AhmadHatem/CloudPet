61c9c97d8c3b77363e71415c4ddba3da51320cd3
Hosam Hany <hosamhany@50-0-48-183.dsl.dynamic.fusionbroadband.com>
2015-03-31 17:38:38 +0200

    Pet Buddy controller

---
+++ app/controllers/buddy_controller.rb
@@ -0,0 +1,37 @@
+class BuddyController < ApplicationController
+	before_action :set_buddy , only: [:show, :edit, :update, :destroy]
+	def index
+		@buddies = Buddy.all
+	end
+
+	def new
+		@buddy = Buddy.new 
+	end
+	def show
+	end
+	def edit
+	end
+	def destroy
+		@buddy.destroy
+		respond_to do |format|
+      format.html { redirect_to feeds_url, notice: 'Pet Buddy was successfully destroyed.' }
+      format.json { head :no_content }
+    	end
+	end
+	def update
+	end
+	def create
+		@buddy= Buddy.new
+		@buddy.register = @register
+		if @buddy.save
+			redirect_to @buddy
+		else
+			render 'new'
+	end 
+	def set_buddy
+		@buddy = buddy.find(params[:id])
+	end 
+	 def buddy_params
+      params.require(:buddy).permit(:user, :register, :can_feed, :can_edit_schedule)
+    end
+end
