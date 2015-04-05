e32a3ad30c18366943c977b2a4ebfc5ce5767b0c
Moaaz Maamoon <moaaz.ali95@gmail.com>
2015-04-05 20:15:49 +0200

    Cat movement added

--- JavaScript/SimOtta.js
+++ JavaScript/SimOtta.js
@@ -1,6 +1,49 @@
+var direction = "right";
 	$(document).ready(
 		function()
 		{
- 
+
 		}
 		);
+
+	 $(document).keydown(
+       	function(e)
+       	{
+	  var key = e.which; 
+	console.log(direction);
+
+if (key == 37)
+{
+
+if(direction == "right")
+{
+	$("div").addClass("reverse-left");
+    direction = "left"; 
+}
+    $(".nyan").addClass("move");
+	$(".nyan").animate({marginLeft: "-=50px"});
+     setTimeout(function(){$(".nyan").removeClass("move");},400);
+
+
+}
+else if (key == 39)
+{
+
+
+   
+
+if(direction == "left")
+{
+	$("div").addClass("reverse-right");
+	direction = "right"; 
+}
+ $(".nyan").addClass("move");
+ 		$(".nyan").animate({marginLeft: "+=50px"});
+        setTimeout(function(){$(".nyan").removeClass("move");},400);
+
+}
+
+
+
+       	}
+       	);
