e2281d54ade22d39cc56c62f02ea4195bd4d6de0
Moaaz Maamoon <moaaz.ali95@gmail.com>
2015-04-06 07:19:17 +0200

    looping simulation without refresh function added

--- JavaScript/SimOtta.js
+++ JavaScript/SimOtta.js
@@ -1,11 +1,19 @@
 var direction = "left";
 var eaten = false;
-var eatenFood = Math.floor(Math.random() * 50);
+var eatenFood = Math.floor(Math.random() * 50) + 2;
+var buttonClicked = false;
+
+
 	$(document).ready(
 		function()
 		{
-		
-$("#deviceInfo").html("<h3> Press F to feed the cat!</h3>");
+			
+	$("#feedButton").click(function(){
+		var e = jQuery.Event("keydown");
+        e.which = 70; 
+        $("#feedButton").trigger(e)
+	});
+$("#deviceInfo").html("<h3> Press F or click on the button to feed the cat!</h3>");
 $("div.nyan").addClass("reverse-left");
 		}
 		);
@@ -30,7 +38,6 @@ if(direction == "right")
 }
 if (key == 39)
 { 
-$(".On").css("marginLeft", "500px"); 
 if(direction == "left")
 {	
 	$("div.nyan").removeClass("reverse-left");
@@ -42,7 +49,7 @@ if(direction == "left")
  setTimeout(function(){$(".nyan").removeClass("move");},500);
 
 }
-if(key == 70 && !eaten)
+if((key == 70 || buttonClicked ) && !eaten )
 {
 	eaten = true;
 $("div.nyan").removeClass("nyan");
@@ -56,10 +63,30 @@ setTimeout(function()
 {
 	$("div").removeClass("NyanLoweringTounge"); 
 	$("div").addClass("nyan");
-	$("#deviceInfo").html("<h3>Your cat ate " + eatenFood + " grams !</h3>" )
-       },6000);
-
-
+	$("#deviceInfo").html("<h3>Your cat ate " + eatenFood + " grams !</h3>" )},6000);
+  setTimeout(
+  function()
+  {  
+    if(direction == "left")
+{	
+	$("div.nyan").removeClass("reverse-left");
+	$("div.nyan").addClass("reverse-right");
+	direction = "right"; 
+}
+ $("div").addClass("NyanMoving");
+ $("div.NyanMoving").animate({marginLeft: "+=487px", easing: 'linear'}, 3000);
+ setTimeout(
+ 	function()
+ 	{
+ 		$("div.NyanMoving").removeClass("NyanMoving"); 
+ 		$("div").addClass("nyan");
+ 		$("div.nyan").removeClass("reverse-right");
+ 		direction = "left";
+	    $("div.nyan").addClass("reverse-left");
+	    eaten = false;
+    }
+    ,3000);
+}, 6000);
 }
 
 	
